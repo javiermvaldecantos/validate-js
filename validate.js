a4a689292f2a939b760c6f0c1feab74b6f9ed5c2
javiermvaldecantos <javiermvaldecantos@gmail.com>
2019-01-31 18:27:29 +1100

    Added three validation functions and some unit tests

---
+++ validate.js
@@ -0,0 +1,224 @@
+var validate = (function () {
+
+    /**
+     * Validates a credit card number
+     * The card number should only contain numbers, dashes or spaces, have a certain length, and pass the Luhn Algorithm.
+     *
+     * @param cardNumber Card number as a string (e.g. "4242 4242 4242 4242")
+     * @param cardType Card type as a string (e.g. "visa")
+     */
+    function isValidCreditCardNumber() {
+        // accept only digits, dashes or spaces
+        if (/[^0-9-\s]+/.test(cardNumber)) return false;
+
+        cardNumber = cardNumber.replace(/\D/g, ""); // remove non-numeric characters
+
+        if( this.isEmpty( cardNumber ) || !this.isNumber(cardNumber) ){
+            return false;
+        }
+
+        // the card number will be valid if it has the correct length according to its type, and if it passes the Luhn Algorithm
+        return self.isValidCreditCardNumberLength(cardNumber, cardType) && self.isValidCreditCardNumberLuhnAlgorithm(cardNumber);
+    }
+
+    /**
+     * _isValidCreditCardNumberLength
+     * 
+     * Validates the length of a credit card number, given its type.
+     * The card number should only contain numbers, dashes or spaces.
+     * Card number lengths taken from the following sources:
+     * https://en.wikipedia.org/wiki/Payment_card_number
+     * http://www.regular-expressions.info/creditcard.html
+     * 
+     * @param cardNumber Card number as a string (e.g. "4242 4242 4242 4242")
+     * @param cardType Card type as a string (e.g. "visa")
+     */
+    function _isValidCreditCardNumberLength(cardNumber, cardType) {
+        // accept only digits, dashes or spaces
+        if (/[^0-9-\s]+/.test(cardNumber)) return false;
+
+        cardNumber = cardNumber.replace(/\D/g, ""); // remove non-numeric characters
+
+        switch(cardType) {
+            case 'amex':
+                return cardNumber.length === 15;
+                break;
+            case 'laser':
+                return cardNumber.length <= 19 && cardNumber >= 16;
+                break;
+            case 'visa':
+                return cardNumber.length === 13 || cardNumber.length === 16;
+                break;
+            case 'mc':
+                return cardNumber.length === 16;
+                break;
+            case 'maestro':
+                return cardNumber.length <= 19 && cardNumber >= 12;
+                break;
+            case 'discover':
+                return cardNumber.length === 16;
+                break;
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * _isValidCreditCardNumberLuhnAlgorithm
+     * Checks that a credit card number fulfills the Luhn Algorithm (checksum)
+     * The card number should only contain numbers, dashes or spaces
+     * 
+     * @param cardNumber Card number as a String (e.g. 4242 4242 4242 4242)
+     */
+    function _isValidCreditCardNumberLuhnAlgorithm(cardNumber) {
+        // accept only digits, dashes or spaces
+        if (/[^0-9-\s]+/.test(cardNumber)) return false;
+
+        var nCheck = 0, nDigit = 0, bEven = false;
+        cardNumber = cardNumber.replace(/\D/g, ""); // remove non-numeric characters
+
+        for (var n = cardNumber.length - 1; n >= 0; n--) {
+            var cDigit = cardNumber.charAt(n),
+                nDigit = parseInt(cDigit, 10);
+
+            if (bEven) {
+                if ((nDigit *= 2) > 9) nDigit -= 9;
+            }
+
+            nCheck += nDigit;
+            bEven = !bEven;
+        }
+
+        return (nCheck % 10) == 0;
+    }
+
+    /**
+     * isValidDate
+     * 
+     * validates a date given the day, month and year
+     * @param day day of the month in DD format
+     * @param month month of the year in MM format
+     * @param year year in YYYY format
+     */
+    function isValidDate(day, month, year) {
+        if(!day || !month || !year) {
+            return false;
+        } else {
+            var maxDaysPerMonth = {
+                '1': 31,
+                '2': ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0) ? 29 : 28,  // taking leap years into account
+                '3': 31,
+                '4': 30,
+                '5': 31,
+                '6': 30,
+                '7': 31,
+                '8': 31,
+                '9': 30,
+                '10': 31,
+                '11': 30,
+                '12': 31
+            }
+            var dayIsInValidRange = day > 0 && day <= maxDaysPerMonth[month];
+            var monthIsInValidRange = month > 0 && month <= 12;
+            var yearIsInValidRange = year > 0 && year <= ((new Date()).getFullYear());
+
+            return dayIsInValidRange && monthIsInValidRange && yearIsInValidRange;
+        }
+    }
+
+    /**
+     * isValidPostcode
+     *
+     * Validates an address postcode. The validation rules are NOT the same for each country.
+     *
+     * If no rules known for a country, assume postcode is valid
+     *
+     * @param postcode: Postcode to be validated
+     * @param country: country code used to validate the postcode. Example: 'au', 'uk', 'us'
+     */
+    function isValidPostcode(postcode, country) {
+        if (typeof postcode !== 'string' || typeof country !== 'string') {
+            return false;
+        }
+
+        var result = true;
+
+        switch(country.toLowerCase()) {
+
+            case 'au':
+            case 'at':
+            case 'be':
+            case 'ch':
+            case 'dk':
+            case 'no':
+            case 'nz':
+            case 'za':  // Australia, Austria, Belgium, Switzerland, Denmark, Norway, New Zealand, South Africa - 1234
+                result = /^\d{4}$/.test(postcode);
+                break;
+
+            case 'de':
+            case 'es':
+            case 'fi':
+            case 'fr':
+            case 'us':  // Germany, Spain, Finland, France and USA - 12345
+                result = /^\d{5}$/.test(postcode);
+                break;
+
+            case 'sg':  // Singapore - 123456
+                result = /^\d{6}$/.test(postcode);
+                break;
+
+            case 'gb':  // United Kingdom - W2 1AB or W12 0AB or W1A 1AA or SW2 1AB or SW12 0AB or SW1A 0AA or GIR 0AA (case insensitive, and the space is optional)
+                var parts = postcode.split(" ");
+                if (parts.length !== 1 && parts.length !== 2) {
+                    result = false; // the postcode can't have more than one space
+                } else {
+                    // get the outward and inward parts of the postcode, in a different way depending on whether the provided postcode has a space or not
+                    var outwardPart = parts.length === 2 ? parts[0] : parts[0].slice(0, -3);
+                    var inwardPart = parts.length === 2 ? parts[1] : parts[0].slice(parts[0].length - 3);
+                    if (/^[A-Z]\d$/i.test(outwardPart)) {    // W2 1AB
+                        result = /^\d[A-Z][A-Z]$/i.test(inwardPart);
+
+                    } else if (/^[A-Z]\d\d$/i.test(outwardPart)) {   // W12 0AB
+                        result = /^\d[A-Z][A-Z]$/i.test(inwardPart) && /^[^CIKMOV]{3}$/i.test(inwardPart);
+
+                    } else {    // W1A 1AA or SW2 1AB or SW12 0AB or SW1A 0AA or GIR 0AA
+                        var outwardPartIsValid = /(^[A-Z]\d[A-Z]$)|(^[A-Z][A-Z]\d$)|(^[A-Z][A-Z]\d\d$)|(^[A-Z][A-Z]\d[A-Z]$)|(^[A-Z]{3}$)/i.test(outwardPart);
+                        var inwardPartIsValid = /^\d[A-Z][A-Z]$/i.test(inwardPart);
+                        result = outwardPartIsValid && inwardPartIsValid;
+                    }
+                }
+                break;
+
+            case 'ca':  // Canada - A1B 2C3 (case insensitive, and the space is optional)
+                result = /^[A-Z]\d[A-Z] ?\d[A-Z]\d$/i.test(postcode);
+                break;
+
+            case 'ex':
+            case 'hk':
+            case 'ie':  // Rest of the world and Ireland - Postcodes not used, so we validate it as a free text field (i.e. valid if it's not empty)
+                result = !!postcode;
+                break;
+
+            case 'nl': // Netherlands - 1234 AB (case insensitive, and the space is optional)
+                result = /^\d{4} ?[A-Z]{2}$/i.test(postcode);
+                break;
+
+            case 'cz':
+            case 'se':  // Czech Republic and Sweden - 123 45 (the space is optional)
+                result = /^\d{3} ?\d{2}$/.test(postcode);
+                break;
+
+            default:
+                result = !!postcode;    // default - postcode will be valid if it's not empty
+        }
+
+        return result;
+    }
+
+    return {
+        isValidCreditCardNumber: isValidCreditCardNumber,
+        isValidDate: isValidDate,
+        isValidPostcode: isValidPostcode
+    }
+})();
---
+++ validate.test.js
@@ -0,0 +1,100 @@
+var validate = require('./validate.js');
+
+test('Credit card numbers are validated correctly', function() {
+    expect(validate.isValidCreditCardNumber('','')).toBe(false);
+    expect(validate.isValidCreditCardNumber('','amex')).toBe(false);
+    expect(validate.isValidCreditCardNumber('','visa')).toBe(false);
+    expect(validate.isValidCreditCardNumber('','mc')).toBe(false);
+    expect(validate.isValidCreditCardNumber('','discover')).toBe(false);
+
+    expect(validate.isValidCreditCardNumber('3400 0000 0000 009','amex')).toBe(true);
+    expect(validate.isValidCreditCardNumber('340000000000009','amex')).toBe(true);
+    expect(validate.isValidCreditCardNumber('4111 1111 1111 1111','visa')).toBe(true);
+    expect(validate.isValidCreditCardNumber('4111111111111111','visa')).toBe(true);
+    expect(validate.isValidCreditCardNumber('5500 0000 0000 0004','mc')).toBe(true);
+    expect(validate.isValidCreditCardNumber('5500000000000004','mc')).toBe(true);
+    expect(validate.isValidCreditCardNumber('6011 0000 0000 0004','discover')).toBe(true);
+    expect(validate.isValidCreditCardNumber('6011000000000004','discover')).toBe(true);
+});
+
+test('Dates are validated properly', function() {
+
+});
+
+test('Address postcodes are validated correctly', function() {
+    expect(validate.isValidPostcode()).toBe(false);
+    expect(validate.isValidPostcode('')).toBe(false);
+    expect(validate.isValidPostcode('1234','au')).toBe(true);
+    expect(validate.isValidPostcode('12 34','au')).toBe(false);
+    expect(validate.isValidPostcode('1234','at')).toBe(true);
+    expect(validate.isValidPostcode('123 4','at')).toBe(false);
+    expect(validate.isValidPostcode('1234','be')).toBe(true);
+    expect(validate.isValidPostcode('1 234','be')).toBe(false);
+    expect(validate.isValidPostcode('A1B 2C3','ca')).toBe(true); // canada postcode well-formatted
+    expect(validate.isValidPostcode('a1b 2c3','ca')).toBe(true); // canada postcode with lowercase letters
+    expect(validate.isValidPostcode('a1B 2c3','ca')).toBe(true); // canada postcode with uppercase and lowercase letters
+    expect(validate.isValidPostcode('a1B2c3','ca')).toBe(true);  // canada postcode without spaces
+    expect(validate.isValidPostcode('A1B2C 3','ca')).toBe(false);// canada postcode with a space in an incorrect place
+    expect(validate.isValidPostcode('1234','ch')).toBe(true);
+    expect(validate.isValidPostcode(' 1234','ch')).toBe(false);
+    expect(validate.isValidPostcode('123 45','cz')).toBe(true);
+    expect(validate.isValidPostcode('12345','cz')).toBe(true);   // Czech posctode with no spaces
+    expect(validate.isValidPostcode('1 2345','cz')).toBe(false);  // Czech postcode with a space in an incorrect place
+    expect(validate.isValidPostcode('12345','de')).toBe(true);
+    expect(validate.isValidPostcode('1234 5','de')).toBe(false);
+    expect(validate.isValidPostcode('1234','dk')).toBe(true);
+    expect(validate.isValidPostcode('1 2 3 4','dk')).toBe(false);
+    expect(validate.isValidPostcode('12345','es')).toBe(true);
+    expect(validate.isValidPostcode('1 2345','es')).toBe(false);
+    expect(validate.isValidPostcode('1234','ex')).toBe(true);
+    expect(validate.isValidPostcode('qwerty','ex')).toBe(true);
+    expect(validate.isValidPostcode('12345','fi')).toBe(true);
+    expect(validate.isValidPostcode('12 345','fi')).toBe(false);
+    expect(validate.isValidPostcode('12345','fr')).toBe(true);
+    expect(validate.isValidPostcode('123 45','fr')).toBe(false);
+    expect(validate.isValidPostcode('1234','hk')).toBe(true);
+    expect(validate.isValidPostcode('1234','ie')).toBe(true);
+    expect(validate.isValidPostcode('1234 AB','nl')).toBe(true);
+    expect(validate.isValidPostcode('1234 aB','nl')).toBe(true); // Dutch postcode with some lowercase letters
+    expect(validate.isValidPostcode('1234 ab','nl')).toBe(true); // Dutch postcode with all lowercase letters
+    expect(validate.isValidPostcode('1234ab','nl')).toBe(true);  // Dutch postcode with all lowercase letters and no spaces
+    expect(validate.isValidPostcode('1234AB','nl')).toBe(true);  // Dutch postcode with no spaces
+    expect(validate.isValidPostcode('1 234AB','nl')).toBe(false);// Dutch postcode with a space in an incorrect place
+    expect(validate.isValidPostcode('123 4AB','nl')).toBe(false);// Dutch postcode with a space in an incorrect place
+    expect(validate.isValidPostcode('1234','no')).toBe(true);
+    expect(validate.isValidPostcode('1234','nz')).toBe(true);
+    expect(validate.isValidPostcode('123 45','se')).toBe(true);
+    expect(validate.isValidPostcode('12345','se')).toBe(true);   // Swedish postcode with no spaces
+    expect(validate.isValidPostcode('12 345','se')).toBe(false); // Swedish postcode with a space in an incorrect place
+    expect(validate.isValidPostcode('123456','sg')).toBe(true);
+    expect(validate.isValidPostcode('123 456','sg')).toBe(false);
+    expect(validate.isValidPostcode('W2 1AB','gb')).toBe(true);
+    expect(validate.isValidPostcode('w2 1Ab','gb')).toBe(true);
+    expect(validate.isValidPostcode('w21ab','gb')).toBe(true);
+    expect(validate.isValidPostcode('W21AB','gb')).toBe(true);
+    expect(validate.isValidPostcode('W21 AB','gb')).toBe(false);
+    expect(validate.isValidPostcode('W12 0AB','gb')).toBe(true);
+    expect(validate.isValidPostcode('w12 0AB','gb')).toBe(true);
+    expect(validate.isValidPostcode('a120ab','gb')).toBe(true);
+    expect(validate.isValidPostcode('W120AB','gb')).toBe(true);
+    expect(validate.isValidPostcode('W120 AB','gb')).toBe(false);
+    expect(validate.isValidPostcode('W1A 1AA','gb')).toBe(true);
+    expect(validate.isValidPostcode('W1a 1aa','gb')).toBe(true);
+    expect(validate.isValidPostcode('w1a1aa','gb')).toBe(true);
+    expect(validate.isValidPostcode('W1 A1AA','gb')).toBe(false);
+    expect(validate.isValidPostcode('SW2 1AB','gb')).toBe(true);
+    expect(validate.isValidPostcode('Sw2 1aB','gb')).toBe(true);
+    expect(validate.isValidPostcode('sw21ab','gb')).toBe(true);
+    expect(validate.isValidPostcode('SW21A B','gb')).toBe(false);
+    expect(validate.isValidPostcode('SW12 0AB','gb')).toBe(true);
+    expect(validate.isValidPostcode('sw12 0AB','gb')).toBe(true);
+    expect(validate.isValidPostcode('sw120ab','gb')).toBe(true);
+    expect(validate.isValidPostcode('SW1 20AB','gb')).toBe(false);
+    expect(validate.isValidPostcode('SW1A 0AA','gb')).toBe(true);
+    expect(validate.isValidPostcode('GIR 0AA','gb')).toBe(true);
+    expect(validate.isValidPostcode('GIR 0aa','gb')).toBe(true);
+    expect(validate.isValidPostcode('gir0aa','gb')).toBe(true);
+    expect(validate.isValidPostcode('gi r0aa','gb')).toBe(false);
+    expect(validate.isValidPostcode('12345','us')).toBe(true);
+    expect(validate.isValidPostcode('1234','za')).toBe(true);
+});
